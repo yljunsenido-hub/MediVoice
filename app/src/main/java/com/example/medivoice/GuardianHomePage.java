b131958d53b3ac9f83befd1af465869f1345ec07
YL - Jun Senido <plukyljunsenido@gmail.com>
2025-10-08 17:01:28 +0800

    SpeechToText: Record History for Guardian

--- app/src/main/java/com/example/medivoice/GuardianHomePage.java
+++ app/src/main/java/com/example/medivoice/GuardianHomePage.java
@@ -1,6 +1,8 @@
 package com.example.medivoice;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,19 @@ public class GuardianHomePage extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button prescriptionButton = findViewById(R.id.prescriptionButton);
+        Button voiceButton = findViewById(R.id.voiceButton);
+
+//        prescriptionButton.setOnClickListener(v -> {
+//            Intent intent = new Intent(GuardianHomePage.this, PrescriptionScanner.class);
+//            startActivity(intent);
+//        });
+
+        voiceButton.setOnClickListener(v -> {
+            Intent voiceIntent = new Intent(GuardianHomePage.this, GuardianRecordSpeechToText.class);
+            startActivity(voiceIntent);
         });
     }
 }
